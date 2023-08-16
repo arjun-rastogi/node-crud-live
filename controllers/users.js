40e056cbb0447c434434374c1494772d5a2110ff
arjun-rastogi <arjun.rastogi2001@gmail.com>
2023-08-16 13:20:32 +0530

    Docker Initialises

--- controllers/users.js
+++ controllers/users.js
@@ -5,89 +5,86 @@ const User = require("../models/user");
 // Get All Users
 
 exports.getUsers = (req, res, next) => {
-    User.findAll()
-    .then(users => {
-        res.status(200).json({users : users});
+  User.findAll()
+    .then((users) => {
+      res.status(200).json({ users: users });
     })
-    .catch(err => console.log(err));
-}
+    .catch((err) => console.log(err));
+};
 
-// Get User By ID 
+// Get User By ID
 
 exports.getUser = (req, res, next) => {
-    const userId = req.params.userId;
-    User.findByPk(userId)
-    .then(user => {
-        if(!user){
-            return res.status(404).json({ message: "User not found" });
-        }
-        res.status(200).json({ user: user});
+  const userId = req.params.userId;
+  User.findByPk(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.status(200).json({ user: user });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
-//Create User 
+//Create User
 exports.createUser = (req, res, next) => {
-    const name = req.body.name;
-    const email = req.body.email;
+  const name = req.body.name;
+  const email = req.body.email;
 
-    User.create({
-        name: name,
-        email: email
+  User.create({
+    name: name,
+    email: email,
+  })
+    .then((result) => {
+      console.log("Created User");
+      res.status(200).json({
+        message: "User Created Successfully.",
+        user: result,
+      });
     })
-    .then(result => {
-        console.log('Created User');
-        res.status(200).json({
-            message: "User Created Successfully.",
-            user: result
-        })
-    })
-    .catch(err =>{
-        console.log(err);
-    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
-
 
 // Update User
 
 exports.updateUser = (req, res, nect) => {
-    const userId = req.params.userId;
-    const updatedName = req.body.name;
-    const updatedEmail = req.body.email;
+  const userId = req.params.userId;
+  const updatedName = req.body.name;
+  const updatedEmail = req.body.email;
 
-    User.findByPk(userId)
-    .then(user => {
-        if(!user){
-            return res.status(404).json({message: "User not found"});
-        }
-        user.name = updatedName;
-        user.email = updatedEmail;
-        return user.save();
+  User.findByPk(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      user.name = updatedName;
+      user.email = updatedEmail;
+      return user.save();
     })
-    .then(result => {
-        res.status(200).json({ message: "User Updated", user: result});
+    .then((result) => {
+      res.status(200).json({ message: "User Updated", user: result });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
-
-// Delete User 
-
+//delete user
 exports.deleteUser = (req, res, next) => {
-    const userId = req.params.id;
-    User.findByPk(userId)
-    .then(user => {
-        if(!user) {
-            return res.status(404).json({ message: "User not found"});
-        }
-        return User.destroy({
-            where : {
-                id: userId
-            }
-        });
+  const userId = req.params.userId;
+  User.findByPk(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found!" });
+      }
+      return User.destroy({
+        where: {
+          id: userId,
+        },
+      });
     })
-    .then(result => {
-        res.status(200).json({message: "User Deleted !!" })
+    .then((result) => {
+      res.status(200).json({ message: "User deleted!" });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
